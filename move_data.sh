649a480ad16a658fbecbce107dd71ebde159be3d
eric <eric.j.blom@gmail.com>
2024-11-24 19:52:37 -0600

    adds new file to move data

--- move_data.sh
+++ move_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp data/output/pwhl_latest_elos.json pwhl-elo-frontend/src/assets/pwhl_latest_elos.json 
-cp data/output/next_five_projections/game_projections_2024-11-18_17:49:04.json pwhl-elo-frontend/src/assets/game_projections.json 
-cp data/output/chartable/chartable_wphl_elos.json pwhl-elo-frontend/src/assets/chartable_wphl_elos.json
+cp data/output/next_five_projections/game_projections_2024-11-24_19:46:18.json pwhl-elo-frontend/src/assets/game_projections.json 
+# cp data/output/chartable/chartable_wphl_elos.json pwhl-elo-frontend/src/assets/chartable_wphl_elos.json
