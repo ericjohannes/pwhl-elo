9220b7422c1201482e8b89f54bd19a2fd7a57134
eric <eric.j.blom@gmail.com>
2024-11-26 18:44:13 -0600

    bolds favored team in projections

--- pwhl-elo-frontend/src/projectionTable.jsx
+++ pwhl-elo-frontend/src/projectionTable.jsx
@@ -20,64 +20,68 @@ export const projectionTable = ()=>(
                 return (
                     <>
                     <h3 key={"date-label" + j} className="quattrocento-regular date-header">{date.date}</h3>
+                    <table key={"projection-table-" + j} className="rating-table projection-table ">
+                        <thead className="fira-code-bold" key={"projection-table-head" + j}>
+                            <tr key={"projection-table-header-row" + j}>
+                                <th key={"projection-table-teams-head" + j} className="team-name table-head-cell">Teams</th>
+                                <th key={"projection-table-home-head" + j} className="team-rating table-head-cell">Home</th>
+                                <th key={"projection-table-rating-head" + j} className="team-rating table-head-cell num-col">Rating</th>
+                                <th key={"projection-table-proj-head" + j} className="team-rating table-head-cell num-col">Win%</th>
+                            </tr>
+                        </thead>
+                        <tbody className="fira-code-regular">
                     {
                         date.games.map((entry, i)=>{
-                    
+                            const notFirstClass = i > 0 ? " not-first-game" : "";
+                            const homeFavored = entry.expected_win_home > entry.expected_win_away;
                             return(
                                 <>
+                                    {/* away team */}
+                                    <tr 
+                                        key={"projection-table-away-row" + i}
+                                        className={"data-row" + notFirstClass}
+                                    >
+                                        <td 
+                                            className="team-name table-left-cell" 
+                                            key={"team-name-" + i}>{convertAndCapitalize(entry.away_team)}
+                                        </td>
+                                        <td className="team-rating" key={"is-away" + i}>Away</td>
+                                        <td 
+                                            className="team-name num-col" 
+                                            key={"away-rating-" + i}>{entry.elo_before_away}
+                                        </td>
+                                        <td 
+                                            className={"team-name num-col" + (homeFavored ? "" : " fira-code-bold")}
+                                            key={"away-projection-" + i}>{roundProjection(entry.expected_win_away)}
+                                        </td>
+
+                                    </tr>
+                                    {/* home team */}
+                                    <tr key={"projection-table-home-row" + i} className="data-row">
+                                        <td 
+                                            className="team-name table-left-cell" 
+                                            key={"team-name-" + i}>{convertAndCapitalize(entry.home_team)}
+                                        </td>
+                                        <td className="team-rating" key={"is-home" + i}>Home</td>
+                                        <td 
+                                            className="team-name num-col" 
+                                            key={"away-rating-" + i}>{entry.elo_before_home}
+                                        </td>
+                                        <td 
+                                            className={"team-name num-col" + (homeFavored ? " fira-code-bold" : "")}
+                                            key={"away-projection-" + i}>{roundProjection(entry.expected_win_home)}
+                                        </td>
+
+                                    </tr>
+
                                     
-                                    <table key={"projection-table-" + i } className="rating-table projection-table ">
-                                        <thead className="fira-code-bold" key={"projection-table-head" + i}>
-                                            <tr key={"projection-table-header-row" + i}>
-                                                <th key={"projection-table-teams-head" + i} className="team-name table-head-cell">Teams</th>
-                                                <th key={"projection-table-home-head" + i} className="team-rating table-head-cell">Home</th>
-                                                <th key={"projection-table-rating-head" + i} className="team-rating table-head-cell num-col">Rating</th>
-                                                <th key={"projection-table-proj-head" + i} className="team-rating table-head-cell num-col">Win%</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody className="fira-code-regular">
-                                            {/* away team */}
-                                            <tr key={"projection-table-away-row" + i} className="data-row">
-                                                <td 
-                                                    className="team-name table-left-cell" 
-                                                    key={"team-name-" + i}>{convertAndCapitalize(entry.away_team)}
-                                                </td>
-                                                <td className="team-rating" key={"is-away" + i}>Away</td>
-                                                <td 
-                                                    className="team-name num-col" 
-                                                    key={"away-rating-" + i}>{entry.elo_before_away}
-                                                </td>
-                                                <td 
-                                                    className="team-name num-col" 
-                                                    key={"away-projection-" + i}>{roundProjection(entry.expected_win_away)}
-                                                </td>
-
-                                            </tr>
-                                            {/* home team */}
-                                            <tr key={"projection-table-home-row" + i} className="data-row">
-                                                <td 
-                                                    className="team-name table-left-cell" 
-                                                    key={"team-name-" + i}>{convertAndCapitalize(entry.home_team)}
-                                                </td>
-                                                <td className="team-rating" key={"is-home" + i}>Home</td>
-                                                <td 
-                                                    className="team-name num-col" 
-                                                    key={"away-rating-" + i}>{entry.elo_before_home}
-                                                </td>
-                                                <td 
-                                                    className="team-name num-col fira-code-regular" 
-                                                    key={"away-projection-" + i}>{roundProjection(entry.expected_win_home)}
-                                                </td>
-
-                                            </tr>
-                                        </tbody>
-
-                                    </table>
                                 </>
                             )
                         })
                         
                     }
+                    </tbody>
+                    </table>
                     </>
                 )
             })
