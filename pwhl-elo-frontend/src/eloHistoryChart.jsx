20fa9fbecfc52a284bf9c1d8d60a6d74c0f59a79
eric <eric.j.blom@gmail.com>
2025-02-07 18:58:53 -0600

    new data and discontinuous chart

--- pwhl-elo-frontend/src/eloHistoryChart.jsx
+++ pwhl-elo-frontend/src/eloHistoryChart.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useMemo, useRef } from "react";
 import * as d3 from "d3";
+import { scaleDiscontinuous, discontinuityRange } from "@d3fc/d3fc-discontinuous-scale";
 
 import chartableWphlElos from "./assets/chartable_wphl_elos.json"
 import {convertAndCapitalize} from "./utils"
 
 
-const NUMTICKSH = 6;
+const NUMTICKSH = 8;
 const NUMTICKSV = 6;
 
 // create a time parser function that works for our time format
@@ -48,6 +49,18 @@ const LineChart = ({ width, height, data }) => {
     // TODO: should be max of data
     const end = new Date(data.max_date);
     const start = new Date(data.min_date); // make this the min of the data
+
+    const offseasonBuffer = 1;
+    const offseasonStart = new Date(2024, 4, 30);
+    const offseasonEnd = new Date(2024, 10, 29);
+    console.log("offseasonStart start", offseasonStart)
+    console.log("offseasonEnd start", offseasonEnd)
+    const offseasonStartBuffed = new Date(offseasonStart.setDate(offseasonStart.getDate() + offseasonBuffer));
+    const offseasonEndBuffed = new Date(offseasonEnd.setDate(offseasonEnd.getDate() - offseasonBuffer));
+
+    console.log("offseasonStart with buffer", offseasonStart)
+    console.log("offseasonEnd with buffer", offseasonEnd)
+
     const domain =[data.min_elo, data.max_elo] // should be [dataMin, dataMax]
     const axesRef = useRef(null);
     const boundsWidth = width - MARGIN.right - MARGIN.left;
@@ -59,6 +72,12 @@ const LineChart = ({ width, height, data }) => {
     const xScale = d3.scaleTime()
         .domain([start, end])
         .range([0, boundsWidth]);
+
+    const xDiscontinuousScale = scaleDiscontinuous(d3.scaleTime())
+        .discontinuityProvider(discontinuityRange([offseasonStartBuffed, offseasonEndBuffed]))
+        .domain([start, end])
+        .range([0, boundsWidth]);
+
     // Y axis
     const yScale = useMemo(() => {
         return d3.scaleLinear().domain(domain).range([boundsHeight, 0]);
@@ -66,22 +85,28 @@ const LineChart = ({ width, height, data }) => {
 
     const lineBuilder = d3
         .line()
-        .x((d) => xScale(new Date(d.date)))
-        .y((d) => yScale(d.elo));
+        .x((d) => xDiscontinuousScale(new Date(d.date)))
+        .y((d) => yScale(d.elo))
+        .defined(function(d) { 
+            if(new Date(d.date) > offseasonStart && new Date(d.date) < offseasonEnd){
+                return false;
+            }
+            return true;
+        });;
 
     // Render the X and Y axis using d3.js, not react
     useEffect(() => {
         const svgElement = d3.select(axesRef.current);
         svgElement.selectAll("*").remove();
-        const xAxisGenerator = d3.axisBottom(xScale);
+        const xAxisGenerator = d3.axisBottom(xDiscontinuousScale);
         svgElement
             .append("g")
             .attr("transform", "translate(0," + boundsHeight + ")")
-            .call(xAxisGenerator.ticks(NUMTICKSH, "%b %e, %Y")); // How many ticks are targeted
+            .call(xAxisGenerator.ticks(NUMTICKSH, "%m/%d/%y")); // How many ticks are targeted
 
         const yAxisGenerator = d3.axisLeft(yScale);
         svgElement.append("g").call(yAxisGenerator.ticks(NUMTICKSV));
-    }, [xScale, yScale, boundsHeight]);
+    }, [xDiscontinuousScale, yScale, boundsHeight]);
 
     // build the lines
 
